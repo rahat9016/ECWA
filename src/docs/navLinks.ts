06d2613a384518321188d055bf88a3db2e850018
rume <rumerx10@gmail.com>
2025-07-21 15:36:42 +0600

    Feat: Checkout Page.

---
+++ src/docs/navLinks.ts
@@ -0,0 +1,9 @@
+export const NavLinks = [
+  { label: 'Phone', href: '/' },
+  { label: 'Mens Cloting', href: '/' },
+  { label: 'Womens Clothing', href: '/' },
+  { label: 'Kids Cloting', href: '/' },
+  { label: 'Electronics', href: '/' },
+  { label: 'Cosmetics', href: '/' },
+  { label: 'Perfumes', href: '/' },
+];
